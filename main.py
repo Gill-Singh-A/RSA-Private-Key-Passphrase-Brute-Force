14daa76616c55ce07b07585175d15415371a3084
Gill-Singh-A <amansg22@iitk.ac.in>
2024-08-11 23:09:23 +0530

    Added Command Line Arguments

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ status_color = {
     ' ': Fore.WHITE
 }
 
+number_of_words = 100000000
+
 def display(status, data, start='', end='\n'):
     print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
 
@@ -23,4 +25,21 @@ def get_arguments(*args):
     return parser.parse_args()[0]
 
 if __name__ == "__main__":
-    pass
+    data = get_arguments(('-l', "--load", "load", "List of Wordlists (seperated by ',')"),
+                         ('-k', "--key", "key", "Path to Private Key Files (seperated by ',')"),
+                         ('-c', "--count", "number_of_words", f"Number of Words to load from a file at one time if the file is large enough that it can't be read at once (Default={number_of_words})"),
+                         ('-w', "--write", "write", "Name of the File for the data to be dumped (default=current data and time)"),)
+    if not data.key:
+        display('-', "Please provide Private Key Files")
+        exit(0)
+    else:
+        data.key = data.key.split(',')
+    if not data.load:
+        display('-', "Please specify Wordlists to Load!")
+        exit(0)
+    if not data.number_of_words:
+        data.number_of_words = number_of_words
+    else:
+        data.number_of_words = int(data.number_of_words)
+    if not data.write:
+        data.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}"
